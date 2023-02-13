26635509a927d4b3940bddf1fc1971a46a5c596b
Hiirsh <47283490+Hiirsh@users.noreply.github.com>
2023-02-13 22:35:11 +0200

    поправил ошибку

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 const Weather = (props) => {
-    return (
-        <div>
-            <p>Location: {props.forecast.city}</p>
-            <p>Temp: {Math.floor(props.forecast.main.temp - 273.15, 1)}</p>
-            <p>Pressure: {props.forecast.main.pressure}</p>
-            <p>Humidity: {props.forecast.main.humidity}</p>
-            <p>Wind: {props.forecast.wind.speed}</p>
-        </div>
-    );
+    if (props && props.forecast) {
+        return (
+            <div>
+                <p>Location: {props.forecast.city}</p>
+                <p>Temp: {Math.floor(props.forecast.main.temp - 273.15, 1)}</p>
+                <p>Pressure: {props.forecast.main.pressure}</p>
+                <p>Humidity: {props.forecast.main.humidity}</p>
+                <p>Wind: {props.forecast.wind.speed}</p>
+            </div>
+        );
+}
 };
 
 export default Weather;
